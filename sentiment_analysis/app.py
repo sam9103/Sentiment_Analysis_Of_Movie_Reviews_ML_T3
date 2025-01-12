8d99f895e4cb8dd354d1e434d0eecd0966443b02
Sam9103 <samruddhideshmukh9103@gmail.com>
2025-01-13 00:29:43 +0530

    Added port

--- sentiment_analysis/app.py
+++ sentiment_analysis/app.py
@@ -46,4 +46,5 @@ def home():
     return render_template('index.html', sentiment=sentiment, error_message=error_message)
 
 if __name__ == "__main__":
+     port = int(os.environ.get("PORT", 5000)) 
     serve(app, host='0.0.0.0', port= port)
